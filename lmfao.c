df53ea9d9736398c58a1307bcb54622b431e5b7c
phrolow <sheludcko.bogdan@gmail.com>
2022-08-19 22:28:46 +0900

    ввод
    теперь не введёшь абы что, но надо костыльнуть точки

--- lmfao.c
+++ lmfao.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #define EPSILON 0.0001
+#define MAXSIZE 40
+
+int get(char k, float *);
 
 main() {
     float a, b, c, d, t;
-    printf("Введите через пробел коэффициенты a, b и c:\n");
-    scanf("%f %f %f", &a, &b, &c);
+
+    if(get('a', &a) || get('b', &b) || get('c', &c)) {
+        printf("Ошибка ввода");
+        return 0;
+    }
+
 
     if(a > (-EPSILON) && a < EPSILON) {
         printf("Ошибка ввода либо коэффициент a слишком мал по модулю");
@@ -23,4 +31,22 @@ main() {
         t = sqrt(d);
         printf("Корни: %f и %f", (-b + t) / (2 * a), (-b - t) / (2 * a));
     }
+}
+
+int get(char k, float *res) {
+    char inp[MAXSIZE];
+    int i = 0;
+
+    printf("Введите коэффициент %c:\n", k);
+    scanf("%s", inp);                                                       //собсно ввод
+
+    while(i < MAXSIZE && inp[i] != '\0') {                                  //проверить, ввёл ли юзверь что-то кроме цифр
+        if ((inp[i] < '0' || inp[i] > '9') && inp[i] != '.')
+            return 1;                                                       //ошибка ввода
+        i++;
+    }
+
+    *res = (float) atof(inp);
+
+    return 0;
 }
