6e752654286ed86131112b1d2f6969f0c2b3d5c6
phrolow <sheludcko.bogdan@gmail.com>
2022-08-26 13:59:18 +0300

    точность

--- kvadrach.cpp
+++ kvadrach.cpp
@@ -84,7 +84,7 @@ int solveEq(double a, double b, double c, double *x1, double *x2) {
 
     double discr = b*b - 4*a*c;
 
-    if(discr < 0) {
+    if(discr < -EPSILON) {
         return NO_ROOT;
     } else if(compare(discr, 0)) {
         *x1 = (-b) / (2*a);
